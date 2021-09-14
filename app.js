4e96b44ed749ade8a9ce793b2957da20bc32a50a
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-14 14:24:45 -0400

    adding logic for team route

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const server = http.createServer(function (request, response) {
 
 	if (request.url === "/game") {
 		// response.end("Check back later for games details")
-		fs.readFile("./public/game.htmls", function (error, data) {
+		fs.readFile("./public/game.html", function (error, data) {
 			if (error) {
 				return response.end(`${error}`);
 			} else {
@@ -27,6 +27,18 @@ const server = http.createServer(function (request, response) {
 				return response.end();
 			}
 		});
+	}
+	if (request.url === "/team") {
+		// response.end("Check back later for teams details")
+		fs.readFile("./public/team.html", function (error, data) {
+			if (error) {
+				return response.end(`${error}`);
+			} else {
+				response.writeHead(200, { "Content-type": "text/html" });
+				response.write(data);
+				return response.end();
+			}
+		});	
 	} else {
 		response.end();
 	}
